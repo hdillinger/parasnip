58ae47c06d88b17a194f9a6dc4c9085f161c0e5c
hannesd <dillinger@biomed.ee.ethz.ch>
2018-03-01 15:17:52 +0100

    refactoring

--- combineFoam.py
+++ combineFoam.py
@@ -1,5 +1,7 @@
-try: paraview.simple
-except: from paraview.simple import *
+try:
+    paraview.simple
+except:
+    from paraview.simple import *
 paraview.simple._DisableFirstRenderCameraReset()
 
 import random
@@ -12,34 +14,28 @@ renderView1 = GetActiveViewOrCreate('RenderView')
 path = 'C:\salome\processor'
 
 for proc in range(procs):
- print proc
- foamfile=path+str(proc)+'\processor'+str(proc)+'.foam' 
- processor0_foam = OpenFOAMReader(FileName = foamfile)
- # hide data in view
- # Hide(processor0_foam, renderView1)
+    foamfile = path + str(proc) + '\processor' + str(proc) + '.foam'
+    processor0_foam = OpenFOAMReader(FileName=foamfile)
 
- processor0_foam.CellArrays = []
- processor0_foam.LagrangianArrays = []
- processor0_foam.MeshRegions = ['internalMesh']
- processor0_foam.PointArrays = []
- # Properties modified on processor0_foam
- processor0_foam.CellArrays = ['U', 'U_0', 'k', 'k_0', 'nuSgs', 'p']
- mr0 = GetDisplayProperties(processor0_foam)
- mr0.DiffuseColor = [random.random(), random.random(), random.random() ]
+    processor0_foam.CellArrays = []
+    processor0_foam.LagrangianArrays = []
+    processor0_foam.MeshRegions = ['internalMesh']
+    processor0_foam.PointArrays = []
+    # Properties modified on processor0_foam
+    processor0_foam.CellArrays = ['U', 'U_0', 'k', 'k_0', 'nuSgs', 'p']
+    mr0 = GetDisplayProperties(processor0_foam)
+    mr0.DiffuseColor = [random.random(), random.random(), random.random()]
 
 # Hide all data objects 
 ss = GetSources()
 for s in ss:
-  Hide(ss[s])
+    Hide(ss[s])
 
 openFOAMReaderlist = []
-for proc in range(1, procs+1):
-	# find source
-	print("proc{0}".format(proc))
-	# openFOAMReader["proc{0}".format(proc)] = FindSource('OpenFOAMReader'+str(proc))
-	openFOAMReaderlist.append(FindSource('OpenFOAMReader'+str(proc)))
-	#openFOAMReader = GetSources('OpenFOAMReader'+str(proc))
-	
+for proc in range(1, procs + 1):
+    # find source
+    openFOAMReaderlist.append(FindSource('OpenFOAMReader' + str(proc)))
+
 # create a new 'Group Datasets'
 groupDatasets1 = GroupDatasets(Input=openFOAMReaderlist)
 
